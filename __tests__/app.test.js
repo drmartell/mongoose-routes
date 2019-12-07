0b7f3053970364f059f895b8124d961c5dbaab9e
Casey Martell <drmartell@gmail.com>
2019-12-07 09:24:17 -0800

    remove unnecessary done statements

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,15 +3,16 @@ const request = require('supertest');
 const app = require('../lib/app');
 require('../lib/utils/connect').connect();
 const disconnect = require('../lib/utils/connect').disconnect;
+
 const testCar = {};
 
 describe('application routes', () => {
-  it('has a home GET route that displays a welcome message', done => request(app)
+  it('has a home GET route that displays a welcome message', () => request(app)
     .get('/')
-    .then(res => { expect(res.text).toEqual('Welcome to the cars database'); done(); })
+    .then(res => expect(res.text).toEqual('Welcome to the cars database'))
   );
   
-  it('has a POST route to add a car to the database', done => request(app)
+  it('has a POST route to add a car to the database', () => request(app)
     .post('/api/post')
     .send({
       make: 'Ford',
@@ -25,31 +26,30 @@ describe('application routes', () => {
     }));
     // store the id for additional testing below
     testCar._id = res.body._id;
-    done();
     })
   );
 
-  it('has a GET route that returns all cars', done => request(app)
+  it('has a GET route that returns all cars', () => request(app)
     .get('/api/cars')
-    .then(res => { expect(res.body).toEqual(expect.any(Array)); done(); })
+    .then(res => expect(res.body).toEqual(expect.any(Array)))
   );
 
-  it('has a GET route that gets a car by id', done => request(app)
+  it('has a GET route that gets a car by id', () => request(app)
     .get(`/api/cars/${testCar._id}`)
-    .then(res => { expect(res.body).toEqual(expect.any(Object)); done(); })
+    .then(res => expect(res.body).toEqual(expect.any(Object)))
   );
 
-  it('has a PUT route that updates a car by id', done => request(app)
+  it('has a PUT route that updates a car by id', () => request(app)
     .put(`/api/put/${testCar._id}`)
     .send({
       make: 'FORD',
     })
-    .then(res => { expect(res.body._id).toEqual(testCar._id); done(); })
+    .then(res => expect(res.body._id).toEqual(testCar._id))
   );
 
-  it('has a DELETE route that deletes by id', done => request(app)
+  it('has a DELETE route that deletes by id', () => request(app)
     .del(`/api/delete/${testCar._id}`)
-    .then(res => { expect(res.body._id).toEqual(testCar._id); done(); })
+    .then(res => expect(res.body._id).toEqual(testCar._id))
   );
 });
 
