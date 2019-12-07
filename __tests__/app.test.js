0656d5e5ada497b7f89b754ab1a1d7c2eda1c690
Casey Martell <drmartell@gmail.com>
2019-12-06 17:00:16 -0800

    adds further tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,40 +3,43 @@ const request = require('supertest');
 const app = require('../lib/app');
 require('../lib/utils/connect')();
 
+const testCar = {};
+
 describe('application routes', () => {
-  it('has a home route that displays a welcome message', () => 
+  it('has a home GET route that displays a welcome message', () => 
     request(app).get('/')
       .then(res => expect(res.text).toEqual('Welcome to the cars database'))
   );
 
-  it('has a home api route that returns all cars', () =>
-    request(app).get('/api/cars')
-      .then(res => {
-        expect(res.body).toEqual({ text: 'hello' });
+  it('has a POST route to add a car to the database', () => 
+    request(app).post('/api/post')
+      .send({
+        make: 'Ford',
+        model: 'Fiesta',
+        year: 2021
+      })
+      .then(res => { expect(res.body).toEqual(expect.objectContaining({
+        make: 'Ford',
+        model: 'Fiesta',
+        year: 2021
+      }));
+      // get the id for additional testing below
+      testCar._id = res.body._id;
       })
   );
 
-  it('has a route to get car by id', () => 
-    request(app).get('/api/cars/:id')
-      .then(res => {
-        expect(res.body)
-      })
-  ); 
+  it('has an GET route that returns all cars', () =>
+    request(app).get('/api/cars')
+      .then(res => expect(res.body).toEqual(expect.any(Array)))
+  );
 
-  it('has a /hello post route', () => 
-    request(app)
-      .post('/hello')
-      .send({
-        name: 'spot',
-        age: 5,
-        weight: '20 lbs'
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          name: 'spot',
-          age: 5,
-          weight: '20 lbs'
-        });
-      })
+  it('has a GET route to get car by id', () => 
+    request(app).get(`/api/cars/${testCar._id}`)
+      .then(res => expect(res.body).toEqual(expect.any(Object)))
+  );
+
+  it('has a PUT route to update a car by id', () =>
+    request(app).get(`/api/put/${testCar._id}`)
+      .then(res => expect(res.body._id).toEqual(testCar._id))
   );
 });
